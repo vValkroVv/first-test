cf2dff5b6c9554ec0d73df7bf9849c9c784a6fef
Валерий Кропотин <valerijkropotin@MacBook-Air-Valerij.local>
2021-04-10 13:10:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({'x': [10, 20, 30, 40],
+                   'y': [100, 200, 300, 400],
+                   'name': ['alpha', 'beta', 'gamma', 'delta']})
+
+x_max = st.slider('Max value of x', float(df['x'].max()))
+
+st.markdown(""" Let's look""")
+
+st.title("My streamlit app")
+
+df
